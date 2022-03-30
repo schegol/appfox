1d35e128ae12d8fef7a89410f5e3437bfda02aef
VladProfitkit <vlad@profitkit.ru>
2022-03-30 21:55:16 +0300

    Подготовка структуры

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
 var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
@@ -17,7 +16,7 @@ var del = require("del");
 var uglify = require("gulp-uglify");
 
 gulp.task("css", function () {
-  return gulp.src("src/sass/style.scss")
+  return gulp.src("source/sass/style.scss")
     .pipe(plumber({
       onError: function (err) {
          console.log(err);
@@ -37,7 +36,7 @@ gulp.task("css", function () {
 });
 
 gulp.task("js", function() {
-  return gulp.src("src/js/script.js")
+  return gulp.src("source/js/script.js")
     .pipe(uglify())
     .pipe(rename("script.min.js"))
     .pipe(gulp.dest("build/js"));
@@ -52,9 +51,9 @@ gulp.task("server", function () {
     ui: false
   });
 
-  gulp.watch("src/sass/**/*.{scss,sass}", gulp.series("css", "refresh"));
-  gulp.watch("src/js/*.js", gulp.series("js", "refresh"));
-  gulp.watch("src/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
@@ -63,7 +62,7 @@ gulp.task("refresh", function (done) {
 });
 
 gulp.task("images", function() {
-  return gulp.src("src/img/**/*.{png,jpg,svg}")
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(gulp.dest("build/img"));
 });
 
@@ -77,7 +76,7 @@ gulp.task("sprite", function () {
 });
 
 gulp.task("html", function () {
-  return gulp.src("src/*.html")
+  return gulp.src("source/*.html")
     .pipe(posthtml([
       include()
     ]))
@@ -90,6 +89,8 @@ gulp.task("copy", function() {
     "source/fonts/**/*.{woff,woff2}",
     "source/img/*.webp",
     "source/js/picturefill.min.js",
+    "source/js/jquery-3.6.0.min.js",
+    "source/js/slick.min.js",
   ], {
     base: "source"
   })
